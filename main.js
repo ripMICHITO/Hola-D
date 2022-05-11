78c2bbdeadda312ae0cb7c13551b616244c3f024
ripMICHITO <101848888+ripMICHITO@users.noreply.github.com>
2022-05-10 21:27:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+nombre_alumno_arry=[] ;
+function submit() {
+    var ver_alumno_array=[] ;
+    for(var i=1;i<=4;i++){
+        var nombre_alumno=document.getElementById("nombre_alumno_"+i).value ; 
+        console.log(nombre_alumno);
+        nombre_alumno_array.push(nombre_alumno);
+    } 
+    console.log(nombre_alumno_array);
+
+    var longitud_nombre_alumnos_array = nombre_alumno_array.length;
+    console.log(longitud_nombre_alumnos_array);
+
+    for (var k = 0; k < longitud_nombre_alumnos_array; k++) 
+    {
+        ver_alumno_array.push("<h4>NOMBRE - "+ nombre_alumno_array[k] + "</h4>");
+        console.log(ver_alumno_array);
+    }
+
+    console.log(ver_alumno_array);
+    document.getElementById("ver_nombre_con_comas").innerHTML = ver_alumno_array;
+
+    var remover_comas = ver_alumno_array.join(" ");
+    console.log(remover_comas);
+    document.getElementById("ver_nombre_sin_comas").innerHTML = remover_comas;
+
+    document.getElementById("botón_enviar").style.display = "none";
+    document.getElementById("botón_ordenar").style.display = "inline-block";
+} 
+function sorting()
+{
+    nombre_alumno_array.sort();
+    console.log(nombre_alumno_array);
+
+    var ver_alumno_array_ordenar = [];
+
+    var longitud_nombre_alumnos_array = nombre_alumno_array.length;
+    console.log(longitud_nombre_alumnos_array);
+
+    for (var k = 0; k < longitud_nombre_alumnos_array; k++) 
+    {
+        ver_alumno_array_ordenar.push("<h4>NOMBRE - " + nombre_alumno_array[k] + "</h4>");
+        console.log(ver_alumno_array_ordenar);
+    }
+
+    var remover_comas = ver_alumno_array_ordenar.join(" ");
+    console.log(remover_comas);
+
+    document.getElementById("ver_nombre_sin_comas").innerHTML = remover_comas;
+}
+
